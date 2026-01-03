1a95f8ef4ddc9ca5faf1ebe6f840729eb5d81dcc
orenkeorenan <bernardusoren123@gmail.com>
2026-01-03 22:34:23 +0900

    first b

--- src/app/_not-found/page.js
+++ src/app/_not-found/page.js
@@ -1,16 +1,8 @@
-import dynamic from 'next/dynamic'
-
-// Dynamically import the client component, no SSR
-const InteractiveWidget = dynamic(
-  () => import('./InteractiveWidget'),
-  { ssr: false } // important: disable server-side rendering
-)
-
 export default function NotFoundPage() {
   return (
     <div style={{ textAlign: 'center', padding: '4rem' }}>
       <h1>404 - Page Not Found</h1>
-      <InteractiveWidget />
+      <p>The page you are looking for does not exist.</p>
     </div>
   )
 }
