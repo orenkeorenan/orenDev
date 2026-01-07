6e4c9a284ff9073e55876b7b18e476625d1fb35c
orenkeorenan <bernardusoren123@gmail.com>
2026-01-07 18:42:24 +0900

    mannaTestingLink

--- src/app/components/FolderContent/Portfolio.js
+++ src/app/components/FolderContent/Portfolio.js
@@ -153,6 +153,7 @@ export default function Portfolio() {
         <ProjectCard
             title={t.manna.title}
             subtitle={t.manna.subtitle}
+            link="https://manna-testing.vercel.app/"
             logo="/Manna_logo.png"
             tags={[
                 "Next.js",
